dbda311f2f9290ae2e5ebd5b4eeb2cfe0457504e
Aurora-sid <3415584933@qq.com>
2024-11-25 13:37:52 +0800

    Final

---
+++ entry/src/main/ets/pages/MessagePages/MySuggestion.js
@@ -0,0 +1,11 @@
+import router from '@ohos.router';
+import promptAction from '@ohos.promptAction';
+import { getUserName } from '../../../utils/data';
+import { getSuggest } from '../../../api/axios';
+struct MySuggestion extends   {
+    constructor() { }
+    build() {
+            .height('100%');
+    }
+}
+//# sourceMappingURL=MySuggestion.js.map
---
+++ entry/src/main/ets/pages/MessagePages/TeacherMessage.js
@@ -0,0 +1,9 @@
+import router from '@ohos.router';
+struct TeacherMessage extends   {
+    constructor() { }
+    build() {
+            .height("100%")
+            .width("100%");
+    }
+}
+//# sourceMappingURL=TeacherMessage.js.map
